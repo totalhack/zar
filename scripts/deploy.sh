e9453ff71c7f7309aeadac77c4b76e3bff333298
totalhack <none@none.com>
2022-08-20 13:39:44 -0400

    bug: fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 # Exit in case of error
 set -e
 
-sh ./scripts/config.sh ${1:-docker-compose.yml} | node ./scripts/repair_compose.mjs | \
+sh ./scripts/config.sh | node ./scripts/repair_compose.mjs | \
 DOCKER_CONTEXT=${DOCKER_CONTEXT?Variable not set} \
 docker stack deploy -c - --with-registry-auth "${STACK_NAME?Variable not set}"
