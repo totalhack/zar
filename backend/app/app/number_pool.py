43d76bfc667cbd0f58eeb30c69adb5c534df5068
totalhack <none@none.com>
2024-10-13 21:46:31 -0400

    increase pool cache and lease times

--- backend/app/app/number_pool.py
+++ backend/app/app/number_pool.py
@@ -1,4 +1,3 @@
-import copy
 import time
 
 import redis
@@ -29,11 +28,11 @@ DAYS = 24 * HOURS
 NUMBER_POOL_CONNECT_TRIES = 5
 # TODO read these from pool configs
 # If number hasn't been renewed in this time, mark expired (eligible to be taken)
-NUMBER_POOL_CACHE_EXPIRATION = 4 * MINUTES
+NUMBER_POOL_CACHE_EXPIRATION = 5 * MINUTES
 # Numbers can get renewed for this amount of time max
 NUMBER_POOL_MAX_RENEWAL_AGE = 7 * DAYS
 # How long we keep call_from -> call_to route contexts cached
-NUMBER_POOL_ROUTE_CACHE_EXPIRATION = 14 * DAYS
+NUMBER_POOL_ROUTE_CACHE_EXPIRATION = 30 * DAYS
 
 LOCK_WAIT_TIMEOUT = 5
 LOCK_HOLD_TIMEOUT = 5
